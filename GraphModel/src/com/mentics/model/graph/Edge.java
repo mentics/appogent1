b5d2111bdeadbf8ca5ec21e5728ba6919336eef1
mentics <36110034+mentics@users.noreply.github.com>
2013-06-02 01:41:18 -0700

    Initial basic model persistence working.

--- GraphModel/src/com/mentics/model/graph/Edge.java
+++ GraphModel/src/com/mentics/model/graph/Edge.java
@@ -1,8 +1,28 @@
 package com.mentics.model.graph;
 
 
+import com.esotericsoftware.kryo.DefaultSerializer;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+@DefaultSerializer(Edge.EdgeSerializer.class)
 public class Edge {
     public static final Edge[] EDGES_EMPTY = new Edge[0];
+
+    public static class EdgeSerializer extends Serializer<Edge> {
+        public void write(Kryo kryo, Output output, Edge obj) {
+            kryo.writeObject(output, obj.target);
+            kryo.writeObject(output, obj.annotationNodes);
+        }
+
+        public Edge read(Kryo kryo, Input input, Class<Edge> type) {
+            Node n = kryo.readObject(input, Node.class);
+            Node[] anns = kryo.readObject(input, Node[].class);
+            return new Edge(n, anns);
+        }
+    }
 
     public final Node target;
     public final Node[] annotationNodes;
--- GraphModel/src/com/mentics/model/graph/Node.java
+++ GraphModel/src/com/mentics/model/graph/Node.java
@@ -1,18 +1,40 @@
 package com.mentics.model.graph;
 
+import com.esotericsoftware.kryo.DefaultSerializer;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+@DefaultSerializer(Node.NodeSerializer.class)
 public class Node {
-    public final int id;
+    public static class NodeSerializer extends Serializer<Node> {
+        public void write(Kryo kryo, Output output, Node obj) {
+            output.writeLong(obj.id);
+            output.writeString(obj.name);
+            kryo.writeObject(output, obj.outgoingEdges);
+        }
+
+        public Node read(Kryo kryo, Input input, Class<Node> type) {
+            long id = input.readLong();
+            String name = input.readString();
+            Edge[] edges = kryo.readObject(input, Edge[].class);
+            return new Node(id, name, edges);
+        }
+    }
+
+    public final long id;
     public final String name;
     public final Edge[] outgoingEdges;
 
 
-    public Node(int id, String name, Edge[] outgoingEdges) {
+    public Node(long id, String name, Edge[] outgoingEdges) {
         this.id = id;
         this.name = name;
         this.outgoingEdges = outgoingEdges;
     }
 
-    public Node(int id, String name) {
+    public Node(long id, String name) {
         this(id, name, Edge.EDGES_EMPTY);
     }
 
